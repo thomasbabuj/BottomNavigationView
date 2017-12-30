bf21070d688f919a7458d0a3047e4cc1972b8d72
Thomas Babu <thomas@interuptive.com>
2017-12-30 11:14:56 +0800

    Switch views when the navigation menu item is selected using Fragments

--- app/src/main/java/com/example/thomasbabuj/bottomnavviewdemo/MainActivity.java
+++ app/src/main/java/com/example/thomasbabuj/bottomnavviewdemo/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.thomasbabuj.bottomnavviewdemo;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        StoreFragment.OnFragmentInteractionListener,
+        GiftsFragment.OnFragmentInteractionListener,
+        CartFragment.OnFragmentInteractionListener,
+        ProfileFragment.OnFragmentInteractionListener
+{
 
+    private static final String APP_LOG = MainActivity.class.getSimpleName();
     private ActionBar toolbar;
     private BottomNavigationView bottomNavigationView;
 
@@ -19,9 +28,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = getSupportActionBar();
+        toolbar.setTitle("Store");
+        loadFragments(new StoreFragment());
 
         bottomNavigationView = findViewById(R.id.navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void loadFragments(Fragment fragment) {
+        // Loading Fragments
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -32,18 +51,31 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_shop:
                     toolbar.setTitle("Shop");
+                    fragment = new StoreFragment();
+                    loadFragments(fragment);
                     return true;
                 case R.id.navigation_gifts:
                     toolbar.setTitle("Gifts");
+                    fragment = new GiftsFragment();
+                    loadFragments(fragment);
                     return true;
                 case R.id.navigation_cart:
                     toolbar.setTitle("Cart");
+                    fragment = new CartFragment();
+                    loadFragments(fragment);
                     return true;
                 case R.id.navigation_profile:
                     toolbar.setTitle("Profile");
+                    fragment = new ProfileFragment();
+                    loadFragments(fragment);
                     return true;
             }
             return false;
         }
     };
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.d(APP_LOG, "OnFramgmentInteraction URI =" + uri.toString());
+    }
 }
